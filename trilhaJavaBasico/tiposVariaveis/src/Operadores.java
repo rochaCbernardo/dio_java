b8787f9b6c6487c9232ae15341ae83705feb5d7d
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-10 15:16:39 -0300

    commit aula 04 operadores, java basico

--- trilhaJavaBasico/tiposVariaveis/src/Operadores.java
+++ trilhaJavaBasico/tiposVariaveis/src/Operadores.java
@@ -1,26 +1,11 @@
 public class Operadores {
     public static void main(String[] args) {
-    /*
-    * se identificar uma string "" dentro de uma passagem de valores irá concatenar
-    * exceto se tiver uma priorização, ex.: "1"+(1+1+1);
-    */
+        int num = 5;
 
-        String concatenacao = "?";
-
-        concatenacao = 1+1+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
+        System.out.println(num);
+        System.out.println(num++);
+        System.out.println(num);
+        System.out.println(++num);
+        
     }
 }
