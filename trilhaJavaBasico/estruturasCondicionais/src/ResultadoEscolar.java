842f5e4f955b3e2ab637574711c42dfa9b9a6076
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-11 20:56:47 -0300

    feat: add ../estruturasCondicionais/ResultadoEscolar.java

---
+++ trilhaJavaBasico/estruturasCondicionais/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 9;
+
+        if(nota >= 7){
+            System.out.println("Aprovado.");
+        }else if(nota >=5 && nota < 7){
+            System.out.println("Prova de recuperação.");
+        }else{
+            System.out.println("Reprovado.");
+        }
+    }
+}
