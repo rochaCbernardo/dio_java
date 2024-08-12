6006ead3be43b6106bdceb808282d5a1036a280d
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-11 21:01:00 -0300

    docs: update ../estruturasCondicionais/ResultadoEscolar.java

--- trilhaJavaBasico/estruturasCondicionais/src/ResultadoEscolar.java
+++ trilhaJavaBasico/estruturasCondicionais/src/ResultadoEscolar.java
@@ -2,12 +2,8 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 9;
 
-        if(nota >= 7){
-            System.out.println("Aprovado.");
-        }else if(nota >=5 && nota < 7){
-            System.out.println("Prova de recuperação.");
-        }else{
-            System.out.println("Reprovado.");
-        }
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
     }
 }
