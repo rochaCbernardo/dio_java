31fa8d4644ab8aebab3826d7536b1bc2af5b1dd6
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-20 20:05:20 -0300

    feat: add Estudante.java file

---
+++ fundamentosProgramacaoOO/classes/src/Estudante.java
@@ -0,0 +1,18 @@
+public class Estudante {
+    String name;
+    int age;
+    String color;
+    String sex;
+
+    public static void eating(){
+
+    }
+
+    public static void drinking(){
+        
+    }
+
+    public static void running(){
+        
+    }
+}
