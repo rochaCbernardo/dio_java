5b51a517227770896c3bcbb610af50e5afd8f6a0
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-20 20:10:23 -0300

    feat: add Escola.java file

---
+++ fundamentosProgramacaoOO/classes/src/Escola.java
@@ -0,0 +1,10 @@
+public class Escola {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        estudante1.name = "Bernardo";
+        estudante1.age = 23;
+        estudante1.color = "Branco";
+        estudante1.sex = "Male";
+
+    }
+}
