8de69fb742e23eaf20ef2c2c63251de9b95af153
rochapbernardo <bernardopolese2809@gmail.com>
2024-08-23 22:12:57 -0300

    docs: update EstadosBrasileiros.java

--- fundamentosProgramacaoOO/enum/src/EstadosBrasileiros.java
+++ fundamentosProgramacaoOO/enum/src/EstadosBrasileiros.java
@@ -1,5 +1,24 @@
-public class EstadosBrasileiros {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public enum EstadosBrasileiros {
+    ESPIRITO_SANTO("ES", "Espírito Santo"),
+    RIO_JANEIRO("RJ", "Rio de Janeiro"),
+    MARANHAO("MA", "Maranhão");
+
+    private String nome;
+    private String sigla;
+
+    private EstadosBrasileiros(String sigla, String nome){
+        this.sigla = sigla;
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getSigla(){
+        return sigla;
+    }
+    
+    public String getNomeMaiusculo(){
+        return nome.toUpperCase();
     }
 }
