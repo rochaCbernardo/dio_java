a16c50a62050f70f74a8f26d4ca8fd1995fb7f32
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-06 20:13:24 -0300

    docs: update ReprodutorMusical.java

--- fundamentosProgramacaoOO/desafioPooIphone/src/ReprodutorMusical.java
+++ fundamentosProgramacaoOO/desafioPooIphone/src/ReprodutorMusical.java
@@ -1,3 +1,11 @@
-public class ReprodutorMusical {
-    
+public interface ReprodutorMusical {
+    private void tocarMusica(){
+        System.out.println("Tocando Música!");
+    }
+    private void pausarMusica(){
+        System.out.println("Música pausada.");
+    }
+    private void selecionarMusica(String Musica){
+        System.out.println("Música selecionada");
+    }
 }
