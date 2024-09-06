eb462e23e342b2efac11d53def7677a93111cfc7
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-06 20:25:16 -0300

    docs: update AparelhoTelefonico.java

--- fundamentosProgramacaoOO/desafioPooIphone/src/AparelhoTelefonico.java
+++ fundamentosProgramacaoOO/desafioPooIphone/src/AparelhoTelefonico.java
@@ -1,3 +1,11 @@
-public class AparelhoTelefonico {
-    
+public interface AparelhoTelefonico {
+    private void ligar(String numero){
+        System.out.println("Realizando chamada!");
+    }
+    private void atender(){
+        System.out.println("Chamada atendida");
+    }
+    private void iniciarCorreioVoz(){
+        System.out.println("Correio eletrônico");
+    }
 }
