7ea27ecc66466fdb456b40522c61c65d1b5bc3d8
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-06 20:27:40 -0300

    docs: update NavegadorInternet.java

--- fundamentosProgramacaoOO/desafioPooIphone/src/NavegadorInternet.java
+++ fundamentosProgramacaoOO/desafioPooIphone/src/NavegadorInternet.java
@@ -1,12 +1,6 @@
 public interface NavegadorInternet {
 
-    private void adicionarNovaAba(){
-        System.out.println("Nova aba adicionada");
-    }
-    private void atualizarPagina(){
-        System.out.println("Página atualizada");
-    }
-    private void exibirPagina(String url){
-        System.out.println("Acesso a página com sucesso");
-    }
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+    public void exibirPagina(String url);
 }
