cbcd3209c6c58dd48a898a21a017576168c96161
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-23 16:54:11 -0300

    feat: add ListaTarefa.java file

---
+++ collectionsJava/src/main/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,35 @@
+package main.list.OperacoesBasicas;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasRemover = new ArrayList<>();
+
+        for(Tarefa t: tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasRemover.add(t);
+            }
+        }
+
+        tarefaList.removeAll(tarefasRemover);
+    }
+
+    public int totalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void descricaoTarefas(){
+        System.out.println(tarefaList);
+    }
+}
