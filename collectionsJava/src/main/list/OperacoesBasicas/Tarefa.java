30f64ce1c6e5391f60086ab7a2fa47a81f79c3d7
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-23 16:41:47 -0300

    docs: update Tarefa.java

--- collectionsJava/src/main/list/OperacoesBasicas/Tarefa.java
+++ collectionsJava/src/main/list/OperacoesBasicas/Tarefa.java
@@ -1,5 +1,13 @@
 package main.list.OperacoesBasicas;
 
 public class Tarefa {
-    
+    private String descricao;
+
+    public Tarefa (String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
 }
