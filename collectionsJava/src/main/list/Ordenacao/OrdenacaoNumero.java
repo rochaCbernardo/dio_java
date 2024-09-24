5ec9c51b2001cdb7fc9ace23a21744101d0f123a
rochapbernardo <bernardopolese2809@gmail.com>
2024-09-24 16:01:44 -0300

    feat: add Numero.java file

--- collectionsJava/src/main/list/Ordenacao/OrdenacaoNumero.java
+++ collectionsJava/src/main/list/Ordenacao/OrdenacaoNumero.java
@@ -1,21 +1,17 @@
 package main.list.Ordenacao;
 import java.util.*;
-public class OrdenacaoNumero implements Comparable{
-    private List<Integer> numeros;
+public class OrdenacaoNumero{
+    private List<Numero> numeros;
 
     public OrdenacaoNumero() {
         this.numeros = new ArrayList<>();
     }
 
-    public int compareToAsc(Integer n) {
-        return Integer.compare(this.numeros, n);
-    }
-
     public void adicionarNumero(int num){
-        numeros.add(num);
+        numeros.add(new Numero(num));
     }
-    public List<Integer> ordenarAscendente(){
-        List<Integer> numerosAscendente = new ArrayList<>(numeros);
+    public List<Numero> ordenarAscendente(){
+        List<Numero> numerosAscendente = new ArrayList<>(numeros);
         Collections.sort(numerosAscendente);
         return numerosAscendente;
     }
